fe8a787a796ebd6acbb89e5f88e1bbd4c6520821
Sergey Sekret <monkeyinsight@gmail.com>
2015-11-28 14:04:08 +0200

    change identation

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var Parser = require('./parser.js'),
-	paths = require('./parse.json');
+    paths = require('./parse.json');
 
 for (var i = 0; i < paths.length; i++) {
-	var parser = new Parser(paths[i].id, paths[i].url, paths[i].regexp, function (data, id) {
-		console.log(id + ': ' + data[1]);
-	});
+    var parser = new Parser(paths[i].id, paths[i].url, paths[i].regexp, function (data, id) {
+        console.log(id + ': ' + data[1]);
+    });
 
-	parser.request();
+    parser.request();
 }
--- parser.js
+++ parser.js
@@ -1,33 +1,33 @@
 var urlparser = require('url'),
-	http = require('http');
+    http = require('http');
 
 function Parser(id, url, regexp, callback) {
-	this.id = id;
-	this.path = urlparser.parse(url);
-	this.regexp = new RegExp(regexp, 'i');
-	this.callback = callback;
+    this.id = id;
+    this.path = urlparser.parse(url);
+    this.regexp = new RegExp(regexp, 'i');
+    this.callback = callback;
 }
 
 Parser.prototype.request = function () {
-	var self = this,
-		options = {
-			host: this.path.host,
-			path: this.path.pathname
-		};
+    var self = this,
+        options = {
+            host: this.path.host,
+            path: this.path.pathname
+        };
 
-	this.req = http.request(options, function (response) {
-		var str = '';
+    this.req = http.request(options, function (response) {
+        var str = '';
 
-		response.on('data', function (chunk) {
-			str += chunk;
-		});
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
 
-		response.on('end', function () {
-			var matches = str.match(self.regexp);
-			return self.callback(matches, self.id);
-		});
-	});
-	this.req.end();
+        response.on('end', function () {
+            var matches = str.match(self.regexp);
+            return self.callback(matches, self.id);
+        });
+    });
+    this.req.end();
 }
 
 module.exports = Parser;
